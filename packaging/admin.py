13afebf59def75d81a643f9098040502c5e4b1cb
premierrufus <banded.gabriel@gmail.com>
2018-09-24 15:44:09 -0400

    added date hierarchy filters and batch filters

--- packaging/admin.py
+++ packaging/admin.py
@@ -7,8 +7,7 @@ from .models import Package, Aging
 class PackageAdmin(admin.ModelAdmin):
 	list_display = ('package_date', 'packager', 'format_type', 'format_qty', 'get_packaged_bbl', 'get_batches', 'the_recipe')
 	date_hierarchy = 'package_date'
-
-
+	filter_horizontal = ('batches',)
 @admin.register(Aging)
 class AgeAdmin(admin.ModelAdmin):
 	list_display = ('barrel', 'start_date', 'end_date', 'the_recipe', 'get_batches')
