4e738eae172b1a1e14728c905feef28524f83247
premierrufus <banded.gabriel@gmail.com>
2018-09-25 12:54:52 -0400

    added production volume calc admin action to packaging admin

--- packaging/admin.py
+++ packaging/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Package, Aging
+from .models import Package, Aging, to_bbl
 # Register your models here.
 
 
@@ -10,6 +10,22 @@ class PackageAdmin(admin.ModelAdmin):
 	filter_horizontal = ('batches',)
 	#search_fields = ['batches__recipe__name']
 	list_filter = ('batches__recipe__name', 'package_date', 'format_type')
+	actions = ['calc_total_production']
+
+
+	def calc_total_production(self, request, queryset):
+		query_bottling = 0
+		query_racking = 0
+		for obj in queryset:
+			if 'CS' in obj.format_type:
+				query_bottling += to_bbl(obj.format_type, obj.format_qty)
+			else:
+				query_racking += to_bbl(obj.format_type, obj.format_qty)
+		self.message_user(request, "Total Bottled Volume: %s" % query_bottling)
+		self.message_user(request, "Total Racked Volume: %s" % query_racking)
+		#print("Total Bottled Volume:", query_bottling)
+		#print("Total Racked Volume:", query_racking)
+	calc_total_production.short_description = "Calculate production volume (bbl) for selected entries"
 
 @admin.register(Aging)
 class AgeAdmin(admin.ModelAdmin):
